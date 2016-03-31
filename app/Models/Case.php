99d956551eeac78562a7e4da4d9071ba6dc2ebc9
wanlei <wanleiterry@sina.com>
2016-03-31 23:04:21 +0800

    案例及分类model

---
+++ app/Models/Case.php
@@ -0,0 +1,14 @@
+<?php 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Anli extends Model {
+	
+	protected $table = 'anli';
+	
+	protected $guarded = ['id'];
+	
+	public $timestamps = true;
+	
+}
