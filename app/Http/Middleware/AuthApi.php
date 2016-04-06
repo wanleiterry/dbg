7638d47e376ba57032d717352ae950bbf21aef41
万磊 <wanlei@mail.weiba66.com>
2016-04-06 14:56:34 +0800

    auth init

---
+++ app/Http/Middleware/AuthApi.php
@@ -0,0 +1,48 @@
+<?php namespace App\Http\Middleware;
+
+use App\Services\UserService;
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+// use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Response;
+
+class AuthApi {
+
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth, UserService $userService)
+    {
+        $this->auth = $auth;
+        $this->userService = $userService;
+    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+        //dd(1);
+        if ($this->auth->guest())
+        {
+            return Response::json('没有权限', 401);
+        }
+        if (!$this->auth->user()->is_admin) {
+            return Response::json('没有权限', 401);
+        }
+        return $next($request);
+	}
+}
---
+++ app/Services/AnliService.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Services;
+
+use App\Models\Anli;
+
+class AnliService {
+
+    protected $offset = 0;
+    protected $limit = 20;
+
+    public function __construct(Anli $anli)
+    {
+        $this->model = $anli;
+    }
+
+    public function getList($params)
+    {
+    	$offset = isset($params['offset']) ? $params['offset'] : $this->offset;
+    	$limit = isset($params['limit']) ? $params['limit'] : $this->limit;
+        $anlis = $this->model->leftJoin('category', 'anli.category_id', '=', 'category.id')
+        			->select('anli.id', 'category.name as cate_name', 'anli.title', 'anli.pic', 'anli.content', 'anli.created_at', 'anli.updated_at')
+        			->skip($offset)
+			        ->take($limit)
+			        ->get();
+        return $anlis;
+    }
+
+    public function get($id)
+    {
+        $anli = $this->model->leftJoin('category', 'anli.category_id', '=', 'category.id')
+        			->select('anli.id', 'category.name as cate_name', 'anli.title', 'anli.pic', 'anli.content', 'anli.created_at', 'anli.updated_at')
+        			->where('anli.id', $id)
+        			->first();
+        return $anli;
+    }
+
+    public function post($params)
+    {
+		if(!isset($params['title'])) {
+			return array('status' => false, 'result' => '未设置标题');
+		}
+		if(!isset($params['category_id'])) {
+			return array('status' => false, 'result' => '未设置分类');
+		}
+		$ins['title'] = $params['title'];
+		$ins['category_id'] = $params['category_id'];
+		$ins['pic'] = isset($params['pic']) ? $params['pic'] : '';
+		$ins['content'] = isset($params['content']) ? $params['content'] : '';
+		if($this->model->create($ins)) {
+			return array('status' => true);
+		} else {
+			return array('status' => false, 'result' => '创建失败');
+		}
+    }
+
+    public function put($id, $params)
+    {
+    	$upd = array();
+    	if(isset($params['title'])) {
+    		$upd['title'] = $params['title'];
+    	}
+    	if(isset($params['category_id'])) {
+    		$upd['category_id'] = $params['category_id'];
+    	}
+    	if(isset($params['pic'])) {
+    		$upd['pic'] = $params['pic'];
+    	}
+    	if(isset($params['content'])) {
+    		$upd['content'] = $params['content'];
+    	}
+		if($this->model->where('id', $id)->update($upd)) {
+			return array('status' => true);
+		} else {
+			return array('status' => false, 'result' => '修改失败');
+		}
+    }
+
+}
